1d7b417c1845ae150040307a7c186844b8c9ca63
AsipheFunda <Asiphefunda@gmail.com>
2023-03-12 07:19:08 -0900

    User POJO class added

---
+++ src/main/java/za/ac/cput/User.java
@@ -0,0 +1,39 @@
+package za.ac.cput;
+
+public class User {
+    private String loginId;
+    private String password;
+
+    public User(String loginId, String password) {
+        this.loginId = loginId;
+        this.password = password;
+    }
+
+    public static User createUser(String s, String userPassword) {
+        return null;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "loginId='" + loginId + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/za/ac/cput/UserTest.java
@@ -0,0 +1,30 @@
+//Author Asiphe Funda 215092317
+
+package za.ac.cput;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    public void test() {
+        User u = new User("User102", "User_password");
+        System.out.println(u);
+    }
+    @Test
+    public void test_fail(){
+        User u = User.createUser( "", "User_password");
+        System.out.println(u.toString());
+        assertNotNull(u);
+    }
+    @Test
+    public void timeout() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
